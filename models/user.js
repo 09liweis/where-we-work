5cb3c6346d417b28da3f63e5c407f28773832804
samliweisen <liweisen007@gmail.com>
2018-01-19 00:07:45 +0000

    create user module

--- models/user.js
+++ models/user.js
@@ -15,5 +15,14 @@ const userSchema = new Schema({
     updated_at: Date
 });
 
+userSchema.pre('save', function(next) {
+    const currentDate = new Date();
+    this.updated_at = currentDate;
+    if (!this.created_at) {
+        this.created_at = currentDate;
+    }
+    next();
+});
+
 var User = mongoose.model('User', userSchema);
 module.exports = User;
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const User = require('../models/user');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -12,8 +14,26 @@ router.get('/signin', function(req, res, next) {
 
 router.post('/signup', function(req, res, next) {
     const email = req.body.email;
+    
+    //check if email exist
+    User.find({email: email}, (err, user) => {
+        if (user) {
+            res.send('User Exist');
+        }
+        if (err) throw err;
+    });
+    
     const password = req.body.password;
-    res.send(req.body);
+    
+    const newUser = User({
+        email: email,
+        password: password
+    });
+    newUser.save(function(err) {
+        if (err) throw err;
+        res.send(newUser);
+    });
+    
 });
 
 module.exports = router;
