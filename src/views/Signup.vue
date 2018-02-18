04651d09244fb89e70902f15a46cf242fa86fe77
samliweisen <liweisen007@gmail.com>
2018-02-18 03:34:57 +0000

    add name and title field for signup

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -2,6 +2,8 @@
     <div id="signup" class="main-inner">
         <h2>Sign Up</h2>
         <mu-text-field label="Email" v-model="user.email" labelFloat fullWidth />
+        <mu-text-field label="Name" v-model="user.name" labelFloat fullWidth />
+        <mu-text-field label="Title" v-model="user.title" labelFloat fullWidth />
         <mu-text-field type="password" v-model="user.password" label="Password" labelFloat fullWidth />
         <mu-raised-button label="Sign Up" class="demo-raised-button" primary v-on:click="handleSignUp" />
     </div>
@@ -11,8 +13,10 @@ export default {
     data() {
         return {
             user: {
-                email: 'weisen.li@hotmail.com',
-                password: '1234'
+                email: '',
+                name: '',
+                title: '',
+                password: ''
             }
         };
     },
@@ -25,11 +29,13 @@ export default {
     methods: {
         handleSignUp() {
             const params = {
+                name: this.user.name,
+                title: this.user.title,
                 email: this.user.email,
                 password: this.user.password
             };
             this.$http.post(this.$store.state.api.signup, params).then(res => {
-                
+                this.$router.push('/editProfile');
             }, res => {
                 //error
             });
