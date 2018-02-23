4bbec8b9f63db2d1a470f754a2e7279d8301073f
samliweisen <liweisen007@gmail.com>
2018-02-23 04:52:46 +0000

    add cors support for different domain

--- routes/comment.js
+++ routes/comment.js
@@ -12,17 +12,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
     const name = req.body.name;
     const content = req.body.content;
     const comment = Comment({
