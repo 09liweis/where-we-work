6a68ec7197109fed9e146c47d44b4f787192ab33
samliweisen <liweisen007@gmail.com>
2018-03-22 22:52:59 +0000

    add map container div to add more global element for map

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Design:
 https://superlist.byaviators.com/
 https://ui8.net/products/places-directory?rel=nl
+https://dribbble.com/shots/2119921-Web-app-Part-I/attachments/384868
--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -1,26 +1,31 @@
 <template>
-    <gmap-map
-        v-if="this.$store.state.map.hasCenter"
-        :center="this.$store.state.map.center"
-        :zoom="15"
-        @click="handleClickMap()"
-    >
-        <gmap-marker
-            :key="index"
-            v-for="(m, index) in this.$store.state.map.markers"
-            :position="m.position"
-            :clickable="true"
-            :draggable="true"
-            @click="handleClickMarker(m)"
-        ></gmap-marker>
-        <gmap-info-window :options="infoWindow.option" :position="infoWindow.position" :opened="infoWindow.open" @closeclick="infoWindow.open=false">
-        <p>{{infoWindow.content.name}}</p>
-        <p v-if="infoWindow.content.website"><a :href="infoWindow.content.website" target="_blank">{{infoWindow.content.website}}</a></p>
-        <p>{{infoWindow.content.title}}</p>
-        <p>{{infoWindow.content.company}}</p>
-        <p>{{infoWindow.content.address}}</p>
-      </gmap-info-window>
-    </gmap-map>
+    <div id="mapContainer">
+        <gmap-map
+            v-if="this.$store.state.map.hasCenter"
+            :center="this.$store.state.map.center"
+            :zoom="15"
+            @click="handleClickMap()"
+        >
+            <gmap-marker
+                :key="index"
+                v-for="(m, index) in this.$store.state.map.markers"
+                :position="m.position"
+                :clickable="true"
+                :draggable="true"
+                @click="handleClickMarker(m)"
+            ></gmap-marker>
+            <gmap-info-window :options="infoWindow.option" :position="infoWindow.position" :opened="infoWindow.open" @closeclick="infoWindow.open=false">
+                <p>{{infoWindow.content.name}}</p>
+                <p v-if="infoWindow.content.website"><a :href="infoWindow.content.website" target="_blank">{{infoWindow.content.website}}</a></p>
+                <p>{{infoWindow.content.title}}</p>
+                <p>{{infoWindow.content.company}}</p>
+                <p>{{infoWindow.content.address}}</p>
+            </gmap-info-window>
+        </gmap-map>
+        <div id="workplaces">
+            <h2>List of current work places</h2>
+        </div>
+    </div>
 </template>
 <script>
 export default {
@@ -76,6 +81,19 @@ export default {
 };
 </script>
 <style>
+#mapContainer {
+    width: 100%;
+    height: 100%;
+    position: absolute !important;
+    top: 0;
+    left: 0;
+}
+#workplaces {
+    position: absolute;
+    right: 0;
+    width: 400px;
+    background-color: rgba(0,0,0,0.7);
+}
 .vue-map-container {
     width: 100%;
     height: 100%;
